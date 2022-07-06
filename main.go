21e180096b67a38e2069be69f6585fac78a30f39
sauron <1369130123@qq.com>
2022-07-06 11:06:33 +0800

    Update main.go

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ import (
 )
 
 var host_port = flag.String("address", ":8000", "listen address, connect address(debug)")
-var nats_address = flag.String("nats", "nats://39.101.140.145:4222", "address of nats server")
+var nats_address = flag.String("nats", "nats://39.101.140.145:4223", "address of nats server")
 var link_num = flag.Int("num", 3, "number of clients(for server) or index of the client(for client)")
 var debug = flag.Bool("debug", false, "run as a socket client")
 var name = flag.String("name", "", "who am I")
